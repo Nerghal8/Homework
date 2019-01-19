46aad33b46d89130b4f9d172bd2eb331613ffdf6
Цепеш <Nesuritus@gmail.com>
2019-01-19 21:35:55 +0300

    Initial commit.

--- TaskEasy2.py
+++ TaskEasy2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #_author_ "Макаров Александр Александрович"
 
 #Задача №1
@@ -20,26 +19,3 @@ for i in Spisok[0:6]:
     else:
         print(i*2)
 
-=======
-#_author_ "Макаров Александр Александрович"
-
-#Задача №1
-fruits=["apple", "banana", "orange", "grapefruit"]
-for fruit in fruits:
-    print("{:>20}".format(fruit))
-
-#Задача №2
-list1 = ("Moon", "Sun", "Earth", "Pluton")
-list2 = ("Venera", "Mars", "Moon", "Pluton")
-New_list = set(list1+list2)
-print(New_list)
-
-#Задача №3
-Spisok = [10, 23, 36, 47, 54, 69]
-for i in Spisok[0:6]:
-    if i%2==0:
-        print(i/4)
-    else:
-        print(i*2)
-
->>>>>>> 6f7b7d1e476f301ff03d360c3ffa469891bc84d8
